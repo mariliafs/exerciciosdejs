a1506d99c2515c1057c905305bef921d29c7be14
Marilia Fernandes de Souza <f.mariliafernandes@gmail.com>
2021-10-05 20:39:29 -0300

    Resolução

--- exercicio01.js
+++ exercicio01.js
@@ -1,15 +1,15 @@
-var nome = "aluno"
-var nota1 = console.log("5")
-var nota2 = console.log("10")
+var nome = "Aluno"
+var nota1 = 9
+var nota2 = 10
 
 var resultado = (nota1 + nota2) / 2
 
-    if (resultado >= 7) {
-        console.log(nome + "  foi aprovada")
-    }
-        if (resultado === 10) {
-        console.log(nome + "  foi aprovada com distinção")
-        }
-    else {
-        console.log(nome + "  foi reprovada")
-    }
+if (resultado === 10) {
+    console.log(nome + " foi aprovado com distinção")
+}
+else if (resultado >= 7) {
+    console.log(nome + " foi aprovado")
+}
+else {
+    console.log(nome + " foi reprovado")
+}
---
+++ exercicio02.js
@@ -0,0 +1,27 @@
+var num1 = 10;
+var num2 = 50;
+var num3 = 3;
+
+if(num1>num2){
+if(num1>num3){
+console.log("Numero 1 é o maior!");
+}else
+console.log("Numero 3 é o maior!");
+}else if(num2>num1){
+if(num2>num3){
+console.log("Numero 2 é o maior!");
+}else
+console.log("Numero 3 é o maior!");
+}
+
+if(num1<num2){
+    if(num1<num3){
+    console.log("Numero 1 é o menor!");
+    }else
+    console.log("Numero 3 é o menor!");
+    }else if(num2<num1){
+    if(num2<num3){
+    console.log("Numero 2 é o menor!");
+    }else
+    console.log("Numero 3 é o menor!");
+    }
