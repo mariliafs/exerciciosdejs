598859db07fd7871ff8ed6cad3faa7aa17a7f127
Marilia Fernandes de Souza <f.mariliafernandes@gmail.com>
2021-10-05 20:01:33 -0300

    Resolução

---
+++ exercicio01.js
@@ -0,0 +1,15 @@
+var nome = "aluno"
+var nota1 = console.log("5")
+var nota2 = console.log("10")
+
+var resultado = (nota1 + nota2) / 2
+
+    if (resultado >= 7) {
+        console.log(nome + "  foi aprovada")
+    }
+        if (resultado === 10) {
+        console.log(nome + "  foi aprovada com distinção")
+        }
+    else {
+        console.log(nome + "  foi reprovada")
+    }
