98b87e274bdd9b61512f793e91518456a06e3cda
Marilia Fernandes de Souza <f.mariliafernandes@gmail.com>
2021-10-05 20:45:53 -0300

    Resolução

---
+++ exercicio03.js
@@ -0,0 +1,15 @@
+var produto1 = 300;
+var produto2 = 450;
+var produto3 = 500;
+
+if(produto1<produto2){
+    if(produto1<produto3){
+    console.log("Produto 1 é o menor, compre este!");
+    }else
+    console.log("Produto 3 é o menor, compre este!");
+    }else if(produto2<produto1){
+    if(produto2<produto3){
+    console.log("Produto 2 é o menor, compre este!");
+    }else
+    console.log("Produto 3 é o menor, compre este!");
+    }
