ec85d8bd615b51cf1ccfc14ab1713dac727a8d53
Marilia Fernandes de Souza <f.mariliafernandes@gmail.com>
2021-10-05 21:18:24 -0300

    Resolução

---
+++ exercicio05.js
@@ -0,0 +1,24 @@
+var num = 7
+
+if (num == 1) {
+    console.log ("Hoje é domingo!")
+}
+    else if (num == 2) {
+    console.log ("Hoje é segunda-feira!")
+    }
+    else if (num == 3) {
+    console.log ("Hoje é terça-feira!")
+    }
+    else if (num == 4) {
+    console.log ("Hoje é quarta-feira!")
+    }
+    else if (num == 5) {
+    console.log ("Hoje é quinta-feira!")
+    }
+    else if (num == 6) {
+    console.log ("Hoje é sexta-feira!")
+    }
+    else {
+    console.log ("Hoje é sábado!")
+    }
+
