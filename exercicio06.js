1a75e67356cea2a36371b0bcfc08603a07630203
Marilia Fernandes de Souza <f.mariliafernandes@gmail.com>
2021-10-05 21:33:47 -0300

    Resolução

---
+++ exercicio06.js
@@ -0,0 +1,22 @@
+var nota1=10
+var nota2=8
+
+var media=(nota1+nota2)/2
+
+if (media >=9) {
+    console.log("conceito A, a nota 1 é " + nota1 + " a nota 2 é " + nota2 + " a média foi " + media + ". Aluno foi aprovado") 
+}
+else if (media >= 7.5) {
+    console.log("conceito B, a nota 1 é " + nota1 + " a nota 2 é " + nota2 + " a média foi " + media + ". Aluno foi aprovado") 
+}
+else if (media >= 6) {
+    console.log("conceito C, a nota 1 é " + nota1 + " a nota 2 é " + nota2 + " a média foi " + media + ". Aluno foi aprovado") 
+}
+else if (media >= 4) {
+    console.log("conceito D, a nota 1 é " + nota1 + " a nota 2 é " + nota2 + " a média foi " + media + ". Aluno foi reprovado") 
+}
+else if (media >= 0) {
+    console.log("conceito E, a nota 1 é " + nota1 + " a nota 2 é " + nota2 + " a média foi " + media + ". Aluno foi reprovado") 
+}
+
+
